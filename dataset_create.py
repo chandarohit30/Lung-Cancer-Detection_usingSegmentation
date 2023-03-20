63209dd7a2ad1315fac150a9c6570f0a2233755a
chanda Rohit <87432545+chandarohit30@users.noreply.github.com>
2023-03-20 13:13:30 -0500

    Update dataset_create.py

--- dataset_create.py
+++ dataset_create.py
@@ -1,39 +1,75 @@
-import lungc
-import os.path
-import cv2
-import random
 import numpy as np
+import random
 
-nocanc=[]
-nocanc_output =[]
-for i in range(1,139):#without folder #run for full data
-	if not os.path.isfile('without/nocanc ('+str(i)+').jpg'):
-		continue
-	print(i)
-	img=cv2.imread('without/nocanc ('+str(i)+').jpg',0)
-	left_feature,right_feature,img_left,img_right=lungc.process_lung(img)
-	
-	nocanc.append(left_feature)
-	nocanc_output.append(0)
+# Define the environment
+num_floors = 4
+max_capacity = 3
+arrival_probs = np.array([0.2, 0.1, 0.2, 0.5])
+departure_probs = np.array([0.25, 0.25, 0.25, 0.25])
+actions = ["UP", "DOWN", "WAIT", "PICKUP/DROPOFF"]
+num_actions = len(actions)
 
-cancl=[]
-cancl_output =[]
-for i in range(1,121):#left folder #run for full data
-	if not os.path.isfile('Left/canc ('+str(i)+').jpg'):
-		continue
-	print(i)
-	img=cv2.imread('Left/canc ('+str(i)+').jpg',0)	
-	left_feature,right_feature,img_left,img_right=lungc.process_lung(img)
-	
-	cancl.append(left_feature)
-	cancl_output.append(1)
+# Define the Q-table
+q_table = np.zeros((num_floors, num_floors, max_capacity+1, num_actions))
 
-random.seed(2)
-dataset=cancl+nocanc
-dataset_output = cancl_output + nocanc_output 
-random.shuffle(dataset)
-random.shuffle(dataset_output)
+# Define the learning parameters
+alpha = 0.1  # Learning rate
+gamma = 0.9  # Discount factor
+epsilon = 0.1  # Exploration rate
 
-with open('datafile.py', 'w') as f:
-	f.write('dataset = %s' % dataset)
-	f.write('\ndataset = %s' % dataset_output)
+# Define a function to choose an action based on the epsilon-greedy policy
+def choose_action(state, epsilon):
+    if random.uniform(0, 1) < epsilon:
+        # Choose a random action
+        return random.randint(0, num_actions-1)
+    else:
+        # Choose the action with the highest Q-value
+        return np.argmax(q_table[state[0], state[1], state[2], :])
+
+# Define a function to simulate the environment and update the Q-table
+def simulate(state, action):
+    reward = 0
+    done = False
+    
+    # Take the action and update the state
+    if action == 0:  # UP
+        if state[0] == num_floors - 1:
+            next_state = state
+        else:
+            next_state = (state[0]+1, state[1], state[2])
+    elif action == 1:  # DOWN
+        if state[0] == 0:
+            next_state = state
+        else:
+            next_state = (state[0]-1, state[1], state[2])
+    elif action == 2:  # WAIT
+        next_state = state
+    elif action == 3:  # PICKUP/DROPOFF
+        num_onboard = state[2]
+        num_waiting = np.random.binomial(max_capacity - num_onboard, arrival_probs[state[1]])
+        num_departing = np.random.binomial(num_onboard, departure_probs[state[0]])
+        next_state = (state[0], state[1], num_onboard + num_waiting - num_departing)
+        reward = 10 * num_departing - 1 * (num_onboard + num_waiting)
+    
+    # Check if the task is done
+    if state == next_state:
+        done = True
+    
+    return next_state, reward, done
+
+
+q_value = q_table[state[0], state[1], state[2], action]
+next_q_value = np.max(q_table[next_state[0], next_state[1], next_state[2], :])
+td_error = reward + gamma * next_q_value - q_value
+q_table[state[0], state[1], state[2], action] += alpha * td_error
+
+state = next_state
+episode_reward += reward
+total_rewards.append(episode_reward)
+
+import matplotlib.pyplot as plt
+
+plt.plot(total_rewards)
+plt.xlabel("Episode")
+plt.ylabel("Total Reward")
+plt.show()
